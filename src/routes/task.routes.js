107de4a5d8366da5d9f8a1858a4e6f4c650e5c95
sirSantana23 <salazarmiguelito23gmail.com>
2021-10-14 10:00:58 -0500

    Second commit

--- src/routes/task.routes.js
+++ src/routes/task.routes.js
@@ -3,7 +3,7 @@ import express from 'express'
 const routes = express.Router()
 
 routes.get("/posts", (req, res)=>{
-    res.send("Bienvenido")
+    res.send("Aqui encontraras diferentes posts, se el primero!")
 })
 
 export default routes
